07bb6c4d2598d1977240551e7ebc77a2ca1db048
PARAS-BYTE <parasji014@gmail.com>
2025-10-24 13:56:39 +0530

    sss

--- index.cjs
+++ index.cjs
@@ -33,7 +33,7 @@ app.use(helmet());
 // ===== CORS must come BEFORE session =====
 app.use(
   cors({
-    origin: "http://localhost:5173", // Your frontend URL
+    origin: ["http://localhost:5173","https://blinkin.vercel.app"], // Your frontend URL
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"]
   })
